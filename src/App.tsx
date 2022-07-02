9335f36fce20924194f233b65f3a50ed7d3e2dee
clesiorods <clesiorodrigues96@gmail.com>
2022-07-02 20:52:57 -0300

    Conteúdo da aula 2 do Ignite Lab 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react"
-import { client } from "./lib/apollo";
-
-
-const GET_LESSIONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
+import { Event } from "./pages/Event";
 
 
 function App() {
 
-  interface Lesson {
-    id: string;
-    title: string;
-  }
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSIONS_QUERY);
-  console.log(data);
 
   return (
     <div className="App">
-      <h1 className="text-5xl font-bold text-violet-500 m-5" >
-        Ola mundo
-      </h1>
-
-      <ul>
-        {data?.lessons.map(lesson => {
-          return <li key={lesson.id} >{lesson.title}</li>
-        })}
-
-      </ul>
-
+      <Event/>
     </div>
   )
 }
